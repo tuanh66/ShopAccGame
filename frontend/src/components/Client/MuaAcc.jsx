df729cf83b368d7f8e219d94ec0b0b175c6ed5fd
Tuan Anh <tnquanganh@gmail.com>
2025-11-25 18:12:52 +0700

    up muaacc

--- frontend/src/components/Client/MuaAcc.jsx
+++ frontend/src/components/Client/MuaAcc.jsx
@@ -1,4 +1,4 @@
-import "../../assets/css/Client/napthe.css";
+import "../../assets/css/Client/muaacc.css";
 
 const MuaAccount = () => {
   return (
@@ -15,6 +15,35 @@ const MuaAccount = () => {
           </a>
         </li>
       </ul>
+      <section className="account-section">
+        <div className="account-header">
+          <h2 className="text-[20px] font-bold leading-[28px]">
+            Danh sách tất cả các Acc Game
+          </h2>
+        </div>
+        <hr />
+        <div className="py-4 text-[15px] font-bold leading-[24px]">
+          Chọn game muốn mua account
+        </div>
+        <div className="account-list">
+          <div className="account-item">
+            <div className="card card-hover">
+              <a href="#" className="p-4 card-body">
+                <div className="card-image">
+                  <img
+                    src="https://static2.mingame89.store/19426957-cdnimaget1/upload/fastcdn/acc-category-shopt1com/82460/images/ACC_FF_T%E1%BB%B0_CH%E1%BB%8CN_%5B41AF991%5D%5B1%5D.gif"
+                    alt="Ảnh game"
+                  />
+                </div>
+                <div class="account-info">
+                  <div class="account-name text-limit">Nick Free Fire tự chọn</div>
+                  <div class="text-[var(--text-link)] text-[13px] font-normal leading-[20px]">Số tài khoản: 1000</div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
